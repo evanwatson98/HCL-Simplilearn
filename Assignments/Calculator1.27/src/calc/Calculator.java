ab3223bad9fec47c88cc4b6a0fe2e22615cbe259
Evan Watson <evanwatson98@gmail.com>
2021-01-27 16:15:52 -0600

    update default readme

--- Assignments/Calculator1.27/src/calc/Calculator.java
+++ Assignments/Calculator1.27/src/calc/Calculator.java
@@ -1,6 +1,26 @@
 package calc;
+import java.io.*; 
 
 public class Calculator {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		InputStream in = null; 
+        try { 
+  
+        	in = new FileInputStream("ABC.txt"); 
+        }catch(Exception excpt) 
+        { 
+            // in case of I/O error 
+            excpt.printStackTrace(); 
+        } finally
+        { 
+            // releasing the resources back to the 
+            // GarbageCollector when closes 
+            if (in!=null) 
+            { 
+                // Use of close() : closing the file 
+                // and releasing resources 
+            	in.close(); 
+            } 
+        } 
 	}
 }
